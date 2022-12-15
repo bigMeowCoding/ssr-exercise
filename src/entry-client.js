dcfdc8b2fed500ad390ab1e0d5fa84232050677f
zhouyijun <zhou495248579@hotmail.com>
2022-12-15 22:54:20 +0800

    chore:客户端vue使用完整版本

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,3 +1,10 @@
-import createApp from "./app.js";
+import { createSSRApp } from "vue/dist/vue.esm-bundler";
+
+function createApp() {
+  return createSSRApp({
+    data: () => ({ count: 1 }),
+    template: `<div @click="count++">{{ count }}</div>`,
+  });
+}
 
 createApp().mount("#app");
